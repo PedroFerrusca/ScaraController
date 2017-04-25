ac13e192681c68ddb226b7b642ebb4dda18483ee
PedroFerrusca <pedro.ferrusca2b@gmail.com>
2017-04-25 00:45:09 -0500

    Add files via upload

---
+++ SCARATestingProgram.py
@@ -0,0 +1,168 @@
+import serial
+import serial_ports
+import comandoArduino
+from Tkinter import Tk, RIGHT, BOTH, RAISED, LEFT, Canvas, NW, Text, END
+from PIL import Image, ImageTk
+import commands
+from ttk import Frame, Button, Style, Combobox, Entry
+from timeit import default_timer as timer 
+from time import sleep
+serialA = None
+state = 0
+
+		
+class App(Frame):
+
+	def __init__(self, parent):
+
+		Frame.__init__(self, parent)
+		self.parent = parent
+		self.initGUI()
+
+	def initGUI(self):
+
+		self.parent.title("SCARA Program by Pedro Ferrusca")
+		#self.openLog(commands.getoutput("date"))
+		self.logName = commands.getoutput("date")
+		self.logName = self.logName.replace(" ", "")
+		self.logName = self.logName.replace(":", "-")
+		self.openLog(self.logName)
+		self.style = Style()
+		self.pack(fill=BOTH, expand=1)
+		self.style.theme_use("default")
+		self.startFrame = Frame(self, relief=RAISED, borderwidth=1)
+		self.startFrame.pack(fill=BOTH, expand=True)
+		self.logo = Image.open("SCARA.jpg")
+		self.RobotImage = ImageTk.PhotoImage(self.logo)
+		self.startCanvas =Canvas(self.startFrame, bg="white", height=self.logo.size[1]+20,
+		 width=self.logo.size[0]+20)
+		self.log = ""
+		self.startCanvas.create_image(10,10, anchor=NW, image=self.RobotImage)
+		self.startCanvas.pack(fill=BOTH, expand=1)
+		self.startButton = Button(self, text="Start", command=self.destroyStart)
+		self.startButton.pack()
+		#self.mainCanvas = Canvas(self.startFrame, bg="white", height=self.logo.size[1]+20, width=500)
+		self.mainText = Text(self.startFrame, width=50, height=50)
+		self.mainFrame = Frame(self.startFrame, borderwidth=1, height=self.logo.size[1]+20, width=500)
+		self.botonera1 = Frame(self.mainFrame, borderwidth=1)
+		s = commands.getoutput("ls /dev/ttyA*")
+		s2 = serial_ports.serialPorts()
+		#self.devices = ["com5", "com6", "com7", "com8"]
+		#self.devices = s.split('\n', s.count('\n'))
+		self.devices = s2
+		self.comboConnect = Combobox(self.botonera1, values=self.devices)
+		self.comboConnect.pack(side=LEFT)
+		self.connectButton = Button(self.botonera1, text="Connect with SCARA Robot", command=self.Connect)
+		self.connectButton.pack(side=LEFT)
+		self.botonera2 = Frame(self.mainFrame, borderwidth=1)
+		self.botonera3 = Frame(self.mainFrame, borderwidth=1)
+		self.botonera4 = Frame(self.mainFrame, borderwidth=1)
+		self.degrees = Entry(self.botonera2)
+		self.degrees2 = Entry(self.botonera3)
+		self.degrees3 = Entry(self.botonera4)
+		self.m1Button = Button(self.botonera2, text="M1", command=self.sendDegreesM1)
+		self.m1Button.pack(side=LEFT)
+		self.m2Button = Button(self.botonera3, text="M2", command=self.sendDegreesM2)
+		self.m2Button.pack(side=LEFT)
+		self.m3Button = Button(self.botonera4, text="M3", command=self.sendDegreesM3)
+		self.m3Button.pack(side=LEFT)
+		self.degrees.pack(side=LEFT)
+		self.degrees2.pack(side=LEFT)
+		self.degrees3.pack(side=LEFT)
+		
+	
+	def destroyStart(self):
+		self.startCanvas.destroy()
+		self.startButton.destroy()
+		self.botonera1.pack()
+		self.botonera2.pack()
+		self.botonera3.pack()
+		self.botonera4.pack()
+		#self.mainCanvas.pack(fill=BOTH, side=LEFT)
+		self.mainText.pack(fill=BOTH, side=LEFT)
+		self.log = 'Send Commands'
+		self.mainText.insert(END, self.log)
+		self.mainFrame.pack(fill=BOTH, side=LEFT)
+
+	def Connect(self):
+		s = "connected to " + self.comboConnect.get()
+		print(s)
+		serialA = serial.Serial(self.comboConnect.get(), 115200, timeout=3)
+		sleep(2)
+		c = CommandArduino(ord('s'), 1, serialA)
+		print(c.send())
+		
+	def sendDegreesM1(self):
+		m1 = int(self.degrees.get())+1000
+		if m1 != 0:
+			print(m1)
+		if serialA is not None:
+			m1command= CommandArduino(m1, 1, serialA)
+			m1command.send()
+			
+				
+		else:
+			print("not connected")
+			self.printMessages("Not Connected, Command attempted: "+ str(m1), self.logName)
+		self.degrees.delete(0, END)
+		
+	def sendDegreesM2(self):
+		m2 = int(self.degrees2.get())
+		m2 += 2000
+		if m2 != 0:
+			print(m2)
+		if serialA is not None:
+			m2command= CommandArduino(m2, 1, serialA)
+			m2command.send()
+			
+				
+		else:
+			print("not connected")
+			self.printMessages("Not Connected, Command attempted: "+ str(m2), self.logName)
+		self.degrees2.delete(0, END)
+
+	def sendDegreesM3(self):
+		m3 = int(self.degrees3.get()) + 3000
+		if m3 != 0:
+			print(m3)
+		if serialA is not None:
+			m3command= CommandArduino(m3, 1, serialA)
+			m3command.send()
+			
+				
+		else:
+			print("not connected")
+			self.printMessages("Not Connected, Command attempted: "+ str(m3) , self.logName)
+		self.degrees3.delete(0, END)
+
+	def sendStop(self):
+		state =0
+
+	def printMessages(self, Message, path):
+		self.log = '\n' + Message
+		self.mainText.insert(END, self.log)
+		path = "Logs/" + path + ".txt"
+		self.currentLog = open(path, "a")
+		self.currentLog.write('\n' + Message + commands.getoutput("date +'%k:%M:%S'"))
+		self.currentLog.close()
+
+	def openLog(self, path):
+		path = "Logs/" + path + ".txt"
+		self.currentLog = open(path, "w")
+		self.currentLog.write("Begin" + '\n' + commands.getoutput("date"))
+		self.currentLog.close()
+		
+
+		
+
+
+
+def main():
+
+	root = Tk()
+	ex = App(root)
+	root.mainloop()
+
+if __name__ == '__main__':
+	main()
+
---
+++ comandoArduino.py
@@ -0,0 +1,30 @@
+
+import serial
+
+class CommandArduino: # Clase comando para indicar los comandos neesarios del robot
+	
+	def __init__(self, code, times, serial1):
+		self.code = code
+		self.times = times
+		self.serialDev = serial1
+		print("command created ") #quitar primer comentario para debuggear
+	def send(self):
+		print("send") #DEBUG
+		self.serialDev.flushInput()
+		self.serialDev.flushOutput()
+		for i in range(0, self.times):
+			self.serialDev.write(chr(self.code))
+			sleep(0.5)
+			print("Sent : " + self.code)
+			res = 0
+			while res ==0:
+				while self.serialDev.inWaiting() <= 0:
+					print self.serialDev.inWaiting()
+					pass
+				res = self.serialDev.read(1)
+				print(res + " OK")
+				if(res != chr(self.code)):
+					self.serialDev.write(chr(self.code))
+				return res
+	def setTimes(self, times):
+		self.times = times
